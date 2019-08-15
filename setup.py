198ac199467df1e83cdb0f5cf5c680dcfc97b367
André Claudino <claudino@d2x.com.br>
2019-08-15 00:00:40 -0300

    Inicializa projeto python

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name='combina-dataset',
+    version='',
+    packages=find_packages(),
+    url='',
+    license='',
+    author='André Claudino',
+    author_email='',
+    description='',
+    install_requires = [
+        'sympy',
+        'scipy'
+    ]
+)
